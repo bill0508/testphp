0af7fc293f6aeb3167405d82940007ab90b7e6d1
Bill <bill0508@163.com>
2020-04-28 10:14:19 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Test for php</title>
+</head>
+<body>
+This is a test page for php!<br>
+<?php echo 'If you see this page, it means it is working!<br>'; ?>
+v1.1
+</body>
+</html>
